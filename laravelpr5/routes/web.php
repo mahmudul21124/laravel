1db272c907746cc0a190f441945d2baf6fa6ec8d
SaifMahabubArnob <mahabub.web404@gmail.com>
2024-11-05 13:18:44 +0600

    05 November 2024

---
+++ laravelpr5/routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    //return view('dashboard');
+    return view('backend.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('/article', ArticleController::class);
+});
+
+// Route::get('/admindashboard', function () {
+//     return view('backend.dashboard');
+// })->middleware(['auth', 'verified'])->name('admin.dashboard');
+
+Route::get('/admin', function () {
+    return view('signin');
+})->name('admin');
+
+Route::get('/admin/signup', function () {
+    return view('signup');
+});
+
+require __DIR__.'/auth.php';
