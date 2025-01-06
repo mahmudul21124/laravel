e8fd22d8083d1d6f7c0c936e8863634a6aad7ec4
SaifMahabubArnob <mahabub.web404@gmail.com>
2025-01-06 13:17:50 +0600

    06 Jan 2025

---
+++ laravel10_breeze_inertia_vue/resources/js/Pages/Post/Index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Welcome to Vue with Laravel</h1>
+        <ul>
+            <li v-for="x in posts" :key="x.id">{{ x.title }}</li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { usePage } from '@inertiajs/vue3';
+
+const {posts} = usePage().props
+
+// defineProps({
+//     posts: {
+//         type: Array,
+//         default: () => [],
+//     }
+// })
+</script>
+
+<!--<script>
+    export default {
+        data() {
+            return {
+                posts: []
+            }
+        },
+    }
+</script> -->
+
+<style scoped></style>
